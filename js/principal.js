c130615e2a7bf0db8dd35bfa3e99d6e1e40129c9
Ivan Poffo Junior <ivan.poffoj@gmail.com>
2021-02-13 16:43:01 -0300

    Aula 4.6: adicionando pacientes a tabela

--- js/principal.js
+++ js/principal.js
@@ -5,7 +5,7 @@ var pacientes = document.querySelectorAll(".paciente");
 
 console.log(pacientes.length)
 
-for (var i = 0; i < pacientes.length; i++){
+for (var i = 0; i < pacientes.length; i++) {
 
   var paciente = pacientes[i];
 
@@ -42,6 +42,47 @@ for (var i = 0; i < pacientes.length; i++){
 };
 
 var botaoAdicionar = document.querySelector('#adicionar-paciente');
-botaoAdicionar.addEventListener('click', function(){
-  console.log('oi, cliquei no botão');
+
+botaoAdicionar.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  var form = document.querySelector('#formAdiciona');
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement('tr');
+
+  var nomeTd = document.createElement('td');
+  var pesoTd = document.createElement('td');
+  var alturaTd = document.createElement('td');
+  var gorduraTd = document.createElement('td');
+  var imcTd = document.createElement('td');
+
+  pacienteTr.classList.add('paciente');
+
+  nomeTd.classList.add('info-nome');
+  pesoTd.classList.add('info-peso');
+  alturaTd.classList.add('info-altura');
+  gorduraTd.classList.add('info-gordura');
+  imcTd.classList.add('info-imc');
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+  imcTd.textContent = imc;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  tabela = document.querySelector('#tabela-pacientes');
+
+  tabela.appendChild(pacienteTr);
+
 });
