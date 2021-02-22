be0cb027f90fbd2d9c1735424de713337e0fa9dd
Ivan Poffo Junior <ivan.poffoj@gmail.com>
2021-02-21 21:08:20 -0300

    validacao do formulario com mensagens informativas

--- js/form.js
+++ js/form.js
@@ -4,41 +4,83 @@ botaoAdicionar.addEventListener('click', function (event) {
 
   var form = document.querySelector('#formAdiciona');
 
-  var nome = form.nome.value;
-  var peso = form.peso.value;
-  var altura = form.altura.value;
-  var gordura = form.gordura.value;
+  var paciente = obtemPacienteDoFormulario(form);
 
-  var pacienteTr = document.createElement('tr');
+  var erros = validaPaciente(paciente);
 
-  var nomeTd = document.createElement('td');
-  var pesoTd = document.createElement('td');
-  var alturaTd = document.createElement('td');
-  var gorduraTd = document.createElement('td');
-  var imcTd = document.createElement('td');
+  if (erros.length > 0) {
+    exibeMensagensDeErros(erros);
+    return;
+  }
 
-  pacienteTr.classList.add('paciente');
-
-  nomeTd.classList.add('info-nome');
-  pesoTd.classList.add('info-peso');
-  alturaTd.classList.add('info-altura');
-  gorduraTd.classList.add('info-gordura');
-  imcTd.classList.add('info-imc');
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-  imcTd.textContent = imc;
-
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  pacienteTr.appendChild(imcTd);
+  var pacienteTr = montaTr(paciente);
 
   tabela = document.querySelector('#tabela-pacientes');
 
   tabela.appendChild(pacienteTr);
 
+  form.reset();
+  
+  document.querySelector('#mensagens-erro').innerHTML = '';
+
 });
+
+function exibeMensagensDeErros(erros) {
+  var ul = document.querySelector('#mensagens-erro');
+
+  ul.innerHTML = '';
+
+  erros.forEach(function (erro) {
+    var li = document.createElement('li');
+    li.textContent = erro;
+    ul.appendChild(li);
+  })
+}
+
+function obtemPacienteDoFormulario(form) {
+
+  paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+  }
+
+  return paciente;
+
+};
+
+function montaTr(paciente) {
+  var pacienteTr = document.createElement('tr');
+  pacienteTr.classList.add('paciente');
+
+  pacienteTr.appendChild(montaTd(paciente.nome, 'info-nome'));
+  pacienteTr.appendChild(montaTd(paciente.peso, 'info-peso'));
+  pacienteTr.appendChild(montaTd(paciente.altura, 'info-altura'));
+  pacienteTr.appendChild(montaTd(paciente.gordura, 'info-gorduta'));
+  pacienteTr.appendChild(montaTd(paciente.imc, 'info-imc'));
+
+  return pacienteTr;
+
+};
+
+function montaTd(dado, classe) {
+  var td = document.createElement('td');
+  td.textContent = dado;
+  td.classList.add(classe)
+
+  return td;
+}
+
+function validaPaciente(paciente) {
+
+  var error = [];
+
+  if (paciente.nome.length == 0) error.push('O nome não pode ser nulo!');
+  if (!validaPeso(paciente.peso)) error.push('Peso invalido!');
+  if (paciente.gordura.length == 0) error.push('A gordura não pode ser nula');
+    if (!validaAltura(paciente.altura)) error.push('Altura invalida!');
+
+  return error;
+}
